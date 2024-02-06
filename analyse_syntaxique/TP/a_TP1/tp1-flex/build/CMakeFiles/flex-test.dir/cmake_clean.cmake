eedcf2d0662c966da650e57278371a2a0ca186b9
typ49 <typ490@gmail.com>
2024-02-06 17:44:12 +0100

    save 06-feb-2024_1744

---
+++ analyse_syntaxique/TP/a_TP1/tp1-flex/build/CMakeFiles/flex-test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flex-test.dir/lexer.c.o"
+  "CMakeFiles/flex-test.dir/main.c.o"
+  "flex-test"
+  "flex-test.pdb"
+  "lexer.c"
+  "lexer.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/flex-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
